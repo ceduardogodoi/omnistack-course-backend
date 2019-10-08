172b9753d9b16a8bab5b02eb58a5b54ae2d7b5db
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2019-10-08 00:06:12 -0300

    Resolve imagens no backend

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 // Add dotenv to read .env keys and values
 const dotenv = require('dotenv');
@@ -22,6 +23,7 @@ mongoose.connect(process.env.DBURL, {
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
